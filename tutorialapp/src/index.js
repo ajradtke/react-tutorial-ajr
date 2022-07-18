c86f3b25b2d03e703a2a9c904ee21f769c571cc5
ajradtke <100101271+ajradtke@users.noreply.github.com>
2022-07-18 10:45:44 -0400

    built first react component from scratch

--- tutorialapp/src/index.js
+++ tutorialapp/src/index.js
@@ -1,5 +1,12 @@
-import React from 'react' // 'react' refers to file in node modules folder, does not need ./
+import React from 'react'; // 'react' refers to file in node modules folder, does not need ./
+import ReactDOM from 'react-dom'; // employs render method to inject js into DOM 'root'
 
-const MyFunction = () => {
-  
+// When you are using create-react-app through gitBash, there are certain errors that pop up for ESlint
+// and BaseConfig. Use VSCode terminal to circumnavigate this.
+const Greeting = () => {
+  return (
+    <h4>this is Aaron and this is my first component, check</h4>
+  )
 }
+
+ReactDOM.render(<Greeting />,document.getElementById('root'));
